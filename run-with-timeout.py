a92ce91d799029eb2f5ef590fb97d667da3a30ad
Matyas Selmeci <matyas@cs.wisc.edu>
2015-12-26 20:48:35 +0000

    run-with-timeout.py: enhance arguments and cleanup (SOFTWARE-1590)
    - HARDTIMEOUT is optional and is specified with a colon after TIMEOUT,
      e.g. "60:30"
    - The signal to use can be specified after the timeout(s)
    - Don't return special codes if the process was killed by the timeout;
      just use 128 + signal (like the shell would)
    - Write to stderr if timeout(s) hit
    
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-info-services/trunk@22265 4e558342-562e-0410-864c-e07659590f8c

--- run-with-timeout.py
+++ run-with-timeout.py
@@ -7,18 +7,69 @@ import errno
 import signal
 
 
-if len(sys.argv) < 4:
-    print "Usage: %s TIMEOUT HARDTIMEOUT COMMAND [ARGS]" % sys.argv[0]
-    print "Run COMMAND, sending it SIGTERM after TIMEOUT seconds."
-    print "If it's still alive, wait for HARDTIMEOUT additional seconds and send it SIGKILL."
+
+def safe_kill(pid, sig):
+    """Send the signal 'sig' to the process with the PID specified by 'pid'.
+    Do nothing if there is no such process.
+
+    """
+    try:
+        return os.kill(pid, sig)
+    except OSError, err:
+        if err.errno != errno.ESRCH:
+            raise
+
+
+def safe_killpg(pgid, sig):
+    """Send the signal 'sig' to the process group with the group id specified
+    by 'pgid'.  Do nothing if there is no such process group.
+
+    """
+    try:
+        return os.killpg(pgid, sig)
+    except OSError, err:
+        if err.errno != errno.ESRCH:
+            raise
+
+
+if len(sys.argv) < 3:
+    print ("""\
+Usage: %s TIMEOUT[:KILL_TIMEOUT] [-SIG] COMMAND [ARGS]"
+
+Run COMMAND, and send it a signal (SIGTERM by default) after TIMEOUT seconds.
+If it's still alive, and KILL_TIMEOUT is specified, wait for KILL_TIMEOUT
+additional seconds and send it SIGKILL.
+
+SIG can be specified as a number or a name from `kill -l`.
+""" % sys.argv[0])
+
     sys.exit()
 
-timeout = float(sys.argv[1])
-hardtimeout = float(sys.argv[2])
-command = sys.argv[3:]
-timeout_return_code = 99
-timeout_signal = signal.SIGTERM
-hardtimeout_signal = signal.SIGKILL
+if ':' in sys.argv[1]:
+    timeout, kill_timeout = sys.argv[1].split(':', 1)
+else:
+    timeout, kill_timeout = sys.argv[1], 0
+
+timeout, kill_timeout = float(timeout), float(kill_timeout)
+
+if sys.argv[2].startswith('-'):
+    # -SIG specified
+    sig = sys.argv.pop(2)[1:].upper()
+
+    # Can be number or string, with or without the 'SIG'
+    if sig.isdigit():
+        timeout_signal = int(sig)
+    elif not sig.startswith('SIG'):
+        timeout_signal = getattr(signal, 'SIG' + sig)
+    else:
+        timeout_signal = getattr(signal, sig)
+
+else:
+    timeout_signal = signal.SIGTERM
+
+
+command = sys.argv[2:]
+
 
 # run main child process in separate session/process group
 setsid_child_pid = os.fork()
@@ -31,31 +82,30 @@ if setsid_child_pid == 0:
 watcher_child_pid = os.fork()
 if watcher_child_pid == 0:
     time.sleep(timeout)
-    try:
-        os.killpg(setsid_child_pid, timeout_signal)
-        if hardtimeout:
-            time.sleep(hardtimeout)
-            os.killpg(setsid_child_pid, hardtimeout_signal)
-    except OSError, err:
-        # we might get "no such process" if the child has already
-        # exited; ignore that, raise others.
-        if err.errno != errno.ESRCH:
-            raise
-    # sleep for long enough for parent to kill us
-    time.sleep(60)
+    sys.stderr.write("Timeout hit, sending signal %d\n" % timeout_signal)
+    safe_killpg(setsid_child_pid, timeout_signal)
+    if kill_timeout:
+        time.sleep(kill_timeout)
+        sys.stderr.write("Kill timeout hit, sending signal %d\n" % signal.SIGKILL)
+        safe_killpg(setsid_child_pid, signal.SIGKILL)
+    sys.exit()
 
 # wait for main child job to complete (or to have been killed)
 _, status = os.waitpid(setsid_child_pid, 0)
 
-# kill watcher if child exited normally (ie, without timeout)
-if os.WIFEXITED(status):
-    return_code = os.WEXITSTATUS(status)
-else:
-    return_code = timeout_return_code
-
-os.kill(watcher_child_pid, signal.SIGTERM)
+# kill watcher once the child exits
+safe_kill(watcher_child_pid, signal.SIGTERM)
 
 # reap watcher
 os.waitpid(watcher_child_pid, 0)
 
+# get the return code based on how the child exited
+if os.WIFEXITED(status):
+    # normal exit
+    return_code = os.WEXITSTATUS(status)
+else:
+    # killed by a signal
+    return_code = 128 + os.WTERMSIG(status)
+
 sys.exit(return_code)
+
